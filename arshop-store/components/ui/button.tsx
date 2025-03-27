a95d61e3c4c60a5eb80e860a0b74c1061c5f95db
Marko Anic <anic.it36.2022@gmail.com>
2025-03-27 21:02:27 +0100

    feat: Add homepage, footer, navbar, and main navigation; implement category fetching and UI components

---
+++ arshop-store/components/ui/button.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import React, { forwardRef } from "react";
+
+export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+    
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type = "button",
+    ...props
+}, ref) => {
+    return (
+        <button 
+            ref={ref} 
+            className={cn(
+                "w-auto rounded-auto bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition",
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+});
+
+Button.displayName = "Button";
+
+export default Button;
